cd4f40878ebf57093e3cbfb50ec763846677b3e9
Rosan Yonghang <roshanlimbu714@gmail.com>
2024-05-22 15:00:50 +0600

    Feature: add movie details screen

--- lib/screens/movie/movie_details.dart
+++ lib/screens/movie/movie_details.dart
@@ -21,19 +21,55 @@ class MovieDetails extends StatefulWidget {
 class _MovieDetailsState extends State<MovieDetails> {
   bool loading = true;
   List<MovieModel> movieSuggestions = [];
+  MovieModel movie = MovieModel(
+      id: -1, title: 'N/A', description: '', rating: 0, runtime: 0, year: 0);
+
   @override
   void initState() {
-    loadMovieSuggestions(widget.movieId.toString());
+    loadData();
   }
 
-  Future<void> loadMovieSuggestions(String id) async {
+  Future<void> loadData() async {
+    await loadMovieDetail();
+    await loadMovieSuggestions();
+    setState(() {
+      loading = false;
+    });
+  }
+
+  Future<void> loadMovieDetail() async {
     final res = await http.get(Uri.parse(
-        'https://yts.mx/api/v2/movie_suggestions.json?movie_id=' + id));
+        'https://yts.mx/api/v2/movie_details.json?movie_id=' +
+            widget.movieId.toString()));
+    // print(jsonDecode(res.body)['data']['movie']);
+
+    final decoded = jsonDecode(res.body)['data']['movie'];
+    print(decoded);
+    MovieModel temp = MovieModel(
+      id: decoded['id'] ?? 0,
+      // title: decoded['title'] ?? '',
+      title: decoded['title'] ?? '',
+      description: decoded['description_intro'] ?? '',
+      rating: decoded['rating'] ?? 0,
+      runtime: decoded['runtime'] ?? 0,
+      year: decoded['year'] ?? 0,
+    );
+
+    setState(() {
+      movie = temp;
+    });
+  }
+
+
+  Future<void> loadMovieSuggestions() async {
+    final res = await http.get(Uri.parse(
+        'https://yts.mx/api/v2/movie_suggestions.json?movie_id=' +
+            widget.movieId.toString()));
     // print(jsonDecode(res.body)['data']['movies']);
     List<MovieModel> tempMovies = [];
 
-    for(final movie in jsonDecode(res.body)['data']['movies']){
-        tempMovies.add(MovieModel.fromJson(movie));
+    for (final movie in jsonDecode(res.body)['data']['movies']) {
+      tempMovies.add(MovieModel.fromJson(movie));
     }
 
     // print(tempMovies);
@@ -45,75 +81,78 @@ class _MovieDetailsState extends State<MovieDetails> {
 
   @override
   Widget build(BuildContext context) {
-    final movie = Provider.of<MovieProvider>(context, listen: false)
-        .getMovieById(widget.movieId);
-
     return Scaffold(
         appBar: AppBar(
           title: Text(movie.title),
         ),
-        body: loading? Center(
-          child:  CircularProgressIndicator(),
-        ): Container(
-          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: Image.network(
-                    movie.img,
-                    height: 300,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 16),
-                  width: double.infinity,
+        body: loading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Container(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: SingleChildScrollView(
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text.rich(
-                        style: TextStyle(
-                          fontSize: 24,
+                      Container(
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
                         ),
-                        TextSpan(
-                          children: <InlineSpan>[
-                            TextSpan(text: 'Length: '),
-                            TextSpan(
-                                text: movie.runtime.toString(),
-                                style: TextStyle(fontWeight: FontWeight.bold))
-                          ],
-                        ),
-                      ),
-                      Text.rich(
-                        style: TextStyle(
-                          fontSize: 24,
-                        ),
-                        TextSpan(
-                          children: <InlineSpan>[
-                            TextSpan(text: 'Year: '),
-                            TextSpan(
-                                text: movie.year.toString(),
-                                style: TextStyle(fontWeight: FontWeight.bold))
-                          ],
+                        child: Image.network(
+                          movie.img,
+                          height: 300,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
                         ),
                       ),
                       Container(
-                        margin: EdgeInsets.only(bottom: 24),
-                        child: Text(movie.description),
-                      ),
-                      MovieSuggestions(movieList: movieSuggestions,)
+                        padding: EdgeInsets.symmetric(vertical: 16),
+                        width: double.infinity,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text.rich(
+                              style: TextStyle(
+                                fontSize: 24,
+                              ),
+                              TextSpan(
+                                children: <InlineSpan>[
+                                  TextSpan(text: 'Length: '),
+                                  TextSpan(
+                                      text: movie.runtime.toString(),
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold))
+                                ],
+                              ),
+                            ),
+                            Text.rich(
+                              style: TextStyle(
+                                fontSize: 24,
+                              ),
+                              TextSpan(
+                                children: <InlineSpan>[
+                                  TextSpan(text: 'Year: '),
+                                  TextSpan(
+                                      text: movie.year.toString(),
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold))
+                                ],
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(bottom: 24),
+                              child: Text(movie.description),
+                            ),
+                            MovieSuggestions(
+                              movieList: movieSuggestions,
+                            )
+                          ],
+                        ),
+                      )
                     ],
                   ),
-                )
-              ],
-            ),
-          ),
-        ));
+                ),
+              ));
   }
 }
--- lib/screens/movie/movies_by_category_screen.dart
+++ lib/screens/movie/movies_by_category_screen.dart
@@ -1,39 +1,75 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../models/movie_model.dart';
 import '../../providers/movie_provider.dart';
 import '../../widgets/movies/movie_tile.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesByCategoryScreen extends StatefulWidget {
   final String category;
-  const MoviesByCategoryScreen({super.key, required this.category});
 
+  const MoviesByCategoryScreen({super.key, required this.category});
 
   @override
   State<MoviesByCategoryScreen> createState() => _MoviesByCategoryScreenState();
 }
 
 class _MoviesByCategoryScreenState extends State<MoviesByCategoryScreen> {
+  List<MovieModel> movieList = [];
+  bool loading = true;
+
+  @override
+  void initState() {
+    loadMoviesByCategory();
+  }
+
+  Future<void> loadMoviesByCategory() async {
+    final res = await http.get(Uri.parse(
+        'https://yts.mx/api/v2/list_movies.json?genre=' + widget.category));
+    // decode data before use
+    // print(jsonDecode(res.body)['data']['movies']);
+    List<MovieModel> tempMovies = [];
+
+    // looping for it to fit in MovieModel format
+    for (final movie in jsonDecode(res.body)['data']['movies']) {
+      tempMovies.add(MovieModel.fromJson(movie));
+    }
+
+    setState(() {
+      movieList = tempMovies;
+      loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<MovieModel> movieList = Provider.of<MovieProvider>(context,listen:false).getMoviesByCategory(widget.category);
-    
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.category),
       ),
-      body: Container(
-        child: movieList.length == 0 ? Center(
-          child: Text('No movies available for this category'),
-        ):ListView.builder(
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            itemCount: movieList.length,
-            itemBuilder: (BuildContext context, int index) {
-              return MovieTile(movie: movieList[index]);;
-            }),
-      ),
+      body: loading
+          ? Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : Container(
+              child: movieList.length == 0
+                  ? Center(
+                      child: Text('No movies available for this category'),
+                    )
+                  : ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: movieList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return MovieTile(movie: movieList[index]);
+                        ;
+                      }),
+            ),
     );
   }
 }
